8b55887f456789fc0af42deda1d9fb188e6ec009
qingbol <qingbol@g.clemson.edu>
2018-12-03 21:11:14 -0500

    modify 7.1.camera_circle to run properly

--- LearnOpengl/1.getting_started/7.1.camera_circle/hello.cpp
+++ LearnOpengl/1.getting_started/7.1.camera_circle/hello.cpp
@@ -1,13 +1,25 @@
-#include <glad/glad.h>
+// Include GLEW
+#ifdef __APPLE__
+#include<OpenGL/gl3.h>
+#include<OpenGL/gl3ext.h>
+#else
+// #include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glext.h>
+#endif
+
+// #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stb_image.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "include/stb_image.h"
+// #include <stb_image.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <learnopengl/filesystem.h>
-#include <learnopengl/shader_m.h>
+#include "include/filesystem.h"
+#include "include/shader_m.h"
 
 #include <iostream>
 
@@ -45,11 +57,11 @@ int main()
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    // if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    // {
+    //     std::cout << "Failed to initialize GLAD" << std::endl;
+    //     return -1;
+    // }
 
     // configure global opengl state
     // -----------------------------
@@ -150,7 +162,8 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
+    // unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("resources/textures/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -172,7 +185,8 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load(FileSystem::getPath("resources/textures/awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
+    // data = stbi_load(FileSystem::getPath("resources/textures/awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
+    data = stbi_load("resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
